af0be0944b00fa35a00cf592d95d67172ba3933a
hua <122794978+huaricecake@users.noreply.github.com>
2023-02-18 18:23:49 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||啦啦啦|生日|快乐|哈哈|哈哈！！|❀|');
+      S.UI.simulate('|#countdown 3||啦啦啦|生日|快乐|哈哈|快乐！！|❀|');
     }
 
     S.Drawing.loop(function () {
